69c584eea431dc4f114ca1964ca4b9d134bd000e
alenavino <alenavino1996@gmail.com>
2024-11-15 15:02:55 +0300

    add tests

--- task_manager/users/tests/test_models.py
+++ task_manager/users/tests/test_models.py
@@ -1,17 +1,17 @@
-# from task_manager.users.tests.testcase import UserTestCase
-# from task_manager.users.models import User
+from task_manager.users.tests.testcase import UserTestCase
+from task_manager.users.models import User
 
 
-# class UserModelTest(UserTestCase):
+class UserModelTest(UserTestCase):
 
-#     def test(self):
-#         self.my_model = User.objects.create(
-#             first_name='Hermione',
-#             last_name='Granger',
-#             username='Jeen',
-#             password='Alohomora')
+    def test(self):
+        self.my_model = User.objects.create(
+            first_name='Hermione',
+            last_name='Granger',
+            username='Jeen',
+            password='Alohomora')
 
-#         self.assertEqual(self.my_model.first_name, 'Hermione')
-#         self.assertEqual(self.my_model.last_name, 'Granger')
-#         self.assertEqual(self.my_model.username, 'Jeen')
-#         self.assertEqual(self.my_model.password, 'Alohomora')
+        self.assertEqual(self.my_model.first_name, 'Hermione')
+        self.assertEqual(self.my_model.last_name, 'Granger')
+        self.assertEqual(self.my_model.username, 'Jeen')
+        self.assertEqual(self.my_model.password, 'Alohomora')
--- task_manager/users/tests/test_views.py
+++ task_manager/users/tests/test_views.py
@@ -7,15 +7,79 @@ class UserViewsTest(UserTestCase):
     def test_index(self):
         response = self.client.get(reverse_lazy('users'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/index.html')
 
-    def test_update_user(self):
+    def test_create_user(self):
         response = self.client.get(reverse_lazy('user_create'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/create.html')
 
         response = self.client.post(reverse_lazy('user_create'), data={
             'first_name': 'Garry',
             'last_name': 'Potter',
             'username': 'Garry',
-            'password': 'ExpectoPatronum'
+            'password1': 'ExpectoPatronum',
+            'password2': 'ExpectoPatronum'
         })
         self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_update_user(self):
+        response = self.client.get(reverse_lazy('user_update', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse_lazy('user_update', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/update.html')
+
+        response = self.client.post(reverse_lazy('user_update', kwargs={
+            'pk': self.user1.id
+            }), data={
+            'first_name': 'Garry',
+            'last_name': 'Potter',
+            'username': 'Garry',
+            'password1': 'ExpectoPatronum',
+            'password2': 'ExpectoPatronum'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('users'))
+
+        self.client.force_login(self.user2)
+        response = self.client.get(reverse_lazy('user_update', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('users'))
+
+    def test_delete_user(self):
+        response = self.client.get(reverse_lazy('user_delete', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+        self.client.force_login(self.user2)
+        response = self.client.get(reverse_lazy('user_delete', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('users'))
+
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse_lazy('user_delete', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/delete.html')
+
+        response = self.client.post(reverse_lazy('user_delete', kwargs={
+            'pk': self.user1.id
+            }))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('users'))
